a8d7de2b204ae855e0c00b5a0496fdfb12953f5e
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2025-04-22 20:01:52 +0530

    added gitignore

--- dojo-backend/src/models/user.model.js
+++ dojo-backend/src/models/user.model.js
@@ -31,12 +31,6 @@ const userSchema = new Schema(
     coverImage:{
       type:String
     },
-    watchHistory:[
-      {
-        type:Schema.Types.ObjectId,
-        ref:"Video",
-      }
-    ],
     password:{
       type:String,
       required:[true,'Password is required'],   
